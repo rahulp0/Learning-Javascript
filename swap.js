0f7d8a0d7ba687461f825a85d36f9036cdb914ca
rahulp0 <rahul.prksh1@gmail.com>
2015-07-07 19:32:56 +0530

    Second commit

---
+++ swap.js
@@ -0,0 +1,14 @@
+  function swap (a,b) {
+        var x = 0; 
+        x = a;
+        a = b;
+        b = x;
+      
+        return [a,b] ;
+    }
+
+   function shuffle ( arr ) {
+     
+        return arr ;
+    }
+        
