24bff068870962ddde126ef79ded869c607f6f56
rahulp0 <rahul.prksh1@gmail.com>
2015-07-08 12:28:07 +0530

    Third commit

--- swap.js
+++ swap.js
@@ -8,7 +8,21 @@
     }
 
    function shuffle ( arr ) {
-     
-        return arr ;
-    }
-        
+         var shuffle;
+       //  shuffle = arr[1];
+         //arr[2] = arr[3];
+         //arr[3] = arr[0]
+        //return arr ;
+    
+   
+     for (var c = 0; c < arr.length; c++) {
+          var t = Math.floor(Math.random ( ) *( arr.length-1) );
+          var x;  
+          x = arr[c];
+          arr[c]=arr[t];
+          arr[t]= x;
+            
+          console.log(t);
+     }   
+     return arr;
+     }//  console.log(t);
